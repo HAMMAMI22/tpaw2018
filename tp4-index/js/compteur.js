b719f68031138e64952e78a97d538bab0c0ff8a8
HAMMAMI22 <32832621+HAMMAMI22@users.noreply.github.com>
2017-11-20 22:25:28 +0100

    Add files via upload

---
+++ tp4-index/js/compteur.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+ function compteur(txt) {
+     document.getElementById('nombre_name').innerHTML=txt.length+' car';
+
+                        }
+
+  function compteur1(txt1) {
+    document.getElementById('nombre_firstname').innerHTML=txt1.length+' car';
+                       
+                                               }
